a1cad70b856f866f429c28a2cdc1ab966f6e3b82
anuudb <anu.udb@gmail.com>
2018-09-21 09:01:47 +0530

    Accessing the container level is done

--- src/operations/env_stats.ts
+++ src/operations/env_stats.ts
@@ -1,5 +1,5 @@
 import { Docker, Options } from "docker-cli-js";
-import path from 'path';
+import path from "path";
 
 let dockerCLI = require("docker-cli-js");
 let DockerOptions = dockerCLI.Options;
@@ -13,23 +13,22 @@ let D_ocker = dockerCLI.Docker;
 const options = new Options(
     /* machineName */ null,
     // /* currentWorkingDirectory */ path.join(__dirname, '..', 'test', 'nginx')
-  );
-  
+);
+
 let docker = new Docker(options);
 
 docker.command("build -t nginximg .").then(function (data) {
     console.log("data = ", data);
-    
 });
-export function d_ps(input: number,req: Request, res: Response) {
+
+export function d_ps(input: number, req: Request, res: Response) {
     // docker.command("ps").then(function (data: any) {
     //     console.log("data = ", data);
     //     return data;
     // });
-
-    console.log('logging path ' + __dirname)
-    console.log('passed param name value is : ' + req.query.name)
-    docker.command("info", function (err, data) {
+    console.log("logging path " + __dirname);
+    console.log("passed param name value is : " + req.query.name);
+    docker.command("ps", function (err, data) {
         console.log("data = ", data);
 
         // return data;
